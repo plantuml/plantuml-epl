11059325f44459f24952faf501d4cb7470d903ea
Arnaud Roques <plantuml@gmail.com>
2020-06-28 14:42:31 +0200

    Import version 1.2020.15

--- src/net/sourceforge/plantuml/version/Version.java
+++ src/net/sourceforge/plantuml/version/Version.java
@@ -43,7 +43,7 @@ public class Version {
 	private static final int MAJOR_SEPARATOR = 1000000;
 
 	public static int version() {
-		return 1202014;
+		return 1202015;
 	}
 
 	public static int versionPatched() {
@@ -92,7 +92,7 @@ public class Version {
 	}
 
 	public static long compileTime() {
-		return 1592662474056L;
+		return 1593344385646L;
 	}
 
 	public static String compileTimeString() {
